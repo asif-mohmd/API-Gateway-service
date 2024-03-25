e1837f11fa2425a006b61cdaac46d922fc4b2735
asif-mohmd <asifasifpsps@gmail.com>
2024-03-25 23:56:42 +0530

    Register working perfectly

--- src/modules/user/controller.ts
+++ src/modules/user/controller.ts
@@ -2,14 +2,36 @@ import { Request, Response, NextFunction } from "express";
 import { UserClient } from "./config/grpc-client/userClient";
 
 export const register = (req: Request, res: Response, next: NextFunction) => {
+    
     UserClient.Register(req.body.formData, (err: Error, result: any) => {
-        console.log(req.body,"jkkghuighjksadfssfsfsfsfsfsfsfsfsfsfsfslssssBiu")
+      console.log(result, "jkkghuighjksadfssfsfsfsfsfsfsfsfsfsfsfslssssBiu");
+      if (err) {
+        res.status(401).json({ message: err });
+        console.log("err in API gateway");
+      } else {
+        console.log("else caseeeeeeeeeee");
+        console.log("----",result,"----------------");
+        // Assuming successful registration, use appropriate status code (e.g., 200)
+        res.status(201).json(result); // Include 'data' in the response
+      }
+    });
+
+  };
+
+  export const login = (req:Request, res:Response, next: NextFunction) =>{
+    UserClient.Login(req.body.loginData,(err:Error,result:any)=>{
+        console.log("login side")
         if(err){
             res.status(401).json({message:err});
-console.log("err in API gateway")
+            console.log("err in login API Gateway")
         }else{
-            console.log("else caseeeeeeeeeee")
-            res.status(201).json(result);
+            console.log("else caseee loginnnn")
+            console.log("------",result,"-----------")
+            res.status(201).json(result)
         }
+
     })
-};
+  }
+
+
+  
--- src/modules/user/route.ts
+++ src/modules/user/route.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction } from "express"; // Import Request and Response types
 import express, { Application } from "express";
-import { register } from "./controller";
+import { login, register } from "./controller";
 
 const userRoute: Application = express();
 
 userRoute.use(express.json()); // Add this line if you want to parse JSON request bodies
 
 userRoute.post("/register", register);
+userRoute.post('/login',login)
 
 export default userRoute;
