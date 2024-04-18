9127ebcd4e7566fbd172a183af2edf0eae075d2f
asif-mohmd <asifasifpsps@gmail.com>
2024-04-19 00:09:17 +0530

    server not starting

--- src/modules/courses/controller.ts
+++ src/modules/courses/controller.ts
@@ -2,13 +2,17 @@ import { Request, Response, NextFunction } from "express";
 
 import { StatusCode } from "../../interfaces/enums";
 import { CourseClient } from "./config/grpc-client/courseClient";
-
+import jwt from "jsonwebtoken"
 
 export default class courseController {
 
 createCourse = (req: Request, res: Response, next: NextFunction) => {
     console.log("rehgiiiiiiiiiiiiiiii",req.body.values)
-      CourseClient.CreateCourse(req.body.values, (err: Error, result: any) => {
+    const instructorData = req.cookies.instructorData;
+    console.log(instructorData)
+    const decoded: any = jwt.verify(instructorData.token, process.env.JWT_SECRET || "");
+      console.log(decoded,"iam deco")
+    CourseClient.CreateCourse(req.body.values, (err: Error, result: any) => {
       if (err) {
         res.status(StatusCode.Unauthorized).json({ message: err });
         console.log("err in API gateway");
