45c2e441666e0c0000fabe3eb31fc9d86a129a34
asif-mohmd <asifasifpsps@gmail.com>
2024-06-14 22:37:21 +0530

    Docker test

--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send('Something broke!');
 });
 
+
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
   RabbitMQClient.initialize();
